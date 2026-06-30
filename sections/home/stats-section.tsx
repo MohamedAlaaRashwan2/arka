8d33dca537e72bcb163f52d8b02bd098bd112e34
Mohamed Alaa <mohamedrashwan.eng@gmail.com>
2026-06-30 15:25:55 +0300

    mt stats

--- sections/home/stats-section.tsx
+++ sections/home/stats-section.tsx
@@ -4,17 +4,17 @@ import { motion } from 'framer-motion';
 import { Statistics } from '@/components/ui/statistics';
 export function StatsSection() {
   return (
-    <section className="relative -mt-20 z-20 mb-20">
+    <section className="relative -mt-10 md:-mt-[7rem] z-20 mb-20">
       <div className="container mx-auto px-4 lg:px-8">
         <motion.div
           initial={{ opacity: 0, y: 40 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{
             once: true,
-            amount: 0.2,
+            amount: 0.5,
           }}          
           transition={{ delay: 0, duration: 0.6 }}
-          className="glass rounded-3xl shadow-arka-lg p-8 lg:p-12"
+          className="glass rounded-3xl shadow-arka-lg p-2 lg:p-12"
         >
           <Statistics />
         </motion.div>
